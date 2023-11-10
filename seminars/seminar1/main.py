ed16ab9974becdb3f31276027cb3d6015827b582
Olga <olga.motovitsa@gmail.com>
2023-11-10 12:58:32 +0300

    add changes

--- seminars/seminar1/main.py
+++ seminars/seminar1/main.py
@@ -5,7 +5,7 @@ import subprocess
 #     result = subprocess.run("cat /etc/os-release", shell=True, stdout=subprocess.PIPE, encoding='utf-8')
 #     out = result.stdout
 #     if "22.04.1" in out and "jammy" in out and result.returncode == 0:
-#         print("SUCCES")
+#         print("SUCCESS")
 #     else:
 #         print("FAIL")
 
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     if result.returncode == 0:
         lst = out.split("\n")
         if 'VERSION="22.04.1 LTS (Jammy Jellyfish)"' in lst and 'VERSION_CODENAME=jammy' in lst:
-            print("SUCCES")
+            print("SUCCESS")
         else:
             print("FAIL")
     else:
